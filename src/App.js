2823efc1089800c9c8a84417685712f0b3b70d53
Dimasio7777 <93773532+Dimasio7777@users.noreply.github.com>
2024-05-25 16:32:48 +0300

    4 Good +QueryId

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
      // window.alert("queryId=" +queryId+ "Total = " + cartItems.reduce((total, item) => total + (item.price * item.quantity)));
     } catch (e) {  window.alert(e.name + ": " + e.message);}
 
-  }, [cartItems]);
+  }, [cartItems,tg]);
   const onCheckout = () => {
     tg.MainButton.text = "Pay :)";
     tg.MainButton.show();
