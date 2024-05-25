db53a01f103f8328dacfdb2e98aa079ed1b6aa84
Dimasio7777 <93773532+Dimasio7777@users.noreply.github.com>
2024-05-25 20:19:50 +0300

    4 Good +QueryId

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback ,useMemo} from "react";
+import { useState, useEffect, useCallback } from "react";
 import {useTelegram} from "./telegram";
 import "./App.css";
 import Card from "./Components/Card/Card";
